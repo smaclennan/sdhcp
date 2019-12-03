dbb3659e843c1a3203a26892784b7bddad5d7e8d
Sean MacLennan <seanm@seanm.ca>
2019-12-02 20:08:27 -0500

    Cleanup

--- compat.h
+++ compat.h
@@ -3,16 +3,9 @@
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define LEN(a) (sizeof(a) / sizeof((a)[0]))
-#define bpdump(p,n) 1
 
 #undef strlcpy
 size_t strlcpy(char *, const char *, size_t);
-
-void weprintf(const char *, ...);
-void eprintf(const char *, ...);
-void enprintf(int, const char *, ...);
-
-/* compat.c */
 
 enum { Broadcast, Unicast };
 
